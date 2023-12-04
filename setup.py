afa1820b770d9090753bac4830908af57f3ef95c
Sylvain <60986961+Sylvain-Deposit@users.noreply.github.com>
2023-12-04 17:31:20 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Chatoyant_Syla", # Replace with your own username
+    name="Chatoyant_Syla",
     version="0.0.1",
     author="Sylvain Rama",
     author_email="rama.sylvain@gmail.com",
     description="Small lib to create colormaps, lego-like.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/driscollis/arithmetic",
+    url="https://github.com/Sylvain-Deposit/Chatoyant",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
